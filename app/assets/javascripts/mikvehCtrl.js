da4235663337f08b4d738e90c3f2af88b62da9de
Shiran Koriat <shiransk@gmail.com>
2016-09-15 16:01:05 -0700

    finish map

--- app/assets/javascripts/mikvehCtrl.js
+++ app/assets/javascripts/mikvehCtrl.js
@@ -2,18 +2,18 @@
   "use strict";
   angular.module("app").controller("mikvehCtrl", function($scope, $http) {
 
+   
+  $scope.setup = function () {
+    $http.get("/api/v1/mikveh.json").then(function(response) {
+    $scope.mikvehs = response.data;
+
     $scope.maps = function(){
-          var locations = [
-      ['Am Echad', 37.2895373, -121.9127035],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+
+    var locations = response.data
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: new google.maps.LatLng(-33.92, 151.25),
+      zoom: 8,
+      center: new google.maps.LatLng(37.4351753, -121.927035),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
@@ -23,26 +23,21 @@
 
     for (i = 0; i < locations.length; i++) {  
       marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
         map: map
       });
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent(locations[i][0]);
+          infowindow.setContent(locations[i].name);
           infowindow.open(map, marker);
         }
       })(marker, i));
     }
     }
 
-
-   
-  $scope.setup = function () {
-    $http.get("/api/v1/mikveh.json").then(function(response) {
-      $scope.mikvehs = response.data;
-    });
     $scope.maps();
+    });
   }
 
   $scope.selectedItemChanged = function(mikvehs) {
